d09514ed51dfd42218a2c4a83058d8206db2b4be
hana <h.anakubo.sys22@morijyobi.ac.jp>
2022-11-08 17:33:51 +0900

    Main.java縺ｮ莉墓ｧ伜､画峩

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("身長:(m):");
-		double height = sc.nextDouble();
-		System.out.println("体�:(kg):");
-		double weight = sc.nextDouble();
+		System.out.println("１人目身�m):");
+		double height1 = sc.nextDouble();
+		System.out.println("１人�体重(kg):");
+		double weight1 = sc.nextDouble();
+		Syste.out.println("２人�身�(m):");
+		double height2 = sc.nextDouble();
+		System.out.println("２人�体�(kg):");
+		double weight2 = sc.nextDouble();
 
-		double BMI = weight / (height * height);
+		double BMI1 = weight / (height * height);
+		double BMI2 = weight / (height * height);
 		
-		System.out.println("BMI:" + String.format("%.2f",BMI));
+		System.out.println("１人目のBMI:" + String.at("%.2f" + BMI1));
+		System.out.println("２人�BMI:" + String.format("%.2f" + BMI2));
 	}
 }
